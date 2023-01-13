96714cecfd98e24ddb8af794adab96997a3d0a20
Isaac Chukwuka <matrix@Isaacs-MacBook.local>
2023-01-13 10:53:03 +0100

    added cors support

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 const app = require("express")();
 const axios = require("axios").default;
+const cors = require("cors");
+
+app.use(cors());
 
 app.get("/api", async (req, res) => {
     try {
         const { data } = await axios.get("https://wakatime.com/api/v1/leaders");
-        res.status(200).json({ data, success: true });
-        console.log(data);
+        res.status(200).json({ ...data, success: true });
     } catch (error) {
         res.status(400).json({ data: null, success: false, msg: "request could not be processed" });
     }
